95c750d8f5a97717ca5b6cdadcc453bc239ffbfe
cerengunaydin <crnn.gnydnn@gmail.com>
2024-05-04 16:39:43 +0300

    second way to open pr

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native'
 const App: React.FC = () => {
   return (
     <SafeAreaView>
-      <Text>This is a pr test commit</Text>
+      <Text>This is a second way to open pr</Text>
     </SafeAreaView>
   )
 }
