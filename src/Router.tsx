f5f35c9aafbad0d5a1e8573f7b51a2b510bed77f
cerengunaydin <crnn.gnydnn@gmail.com>
2024-05-04 16:49:11 +0300

    i changed text in app

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native'
 const App: React.FC = () => {
   return (
     <SafeAreaView>
-      <Text>This is a second way to open pr</Text>
+      <Text>This is a third way to open pr</Text>
     </SafeAreaView>
   )
 }
