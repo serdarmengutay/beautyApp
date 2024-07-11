e5f337ef8658019457a32d216db6a1a2e6706aea
Serdar Mengutay <serdarmngty03@gmail.com>
2024-07-11 18:03:32 +0300

    login page

--- src/screens/Auth/Login/Login.js
+++ src/screens/Auth/Login/Login.js
@@ -1,10 +1,21 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
 import {useState} from 'react';
+import keyboardDismiss from '../../../utils/keyboardDismiss';
 import {theme} from '../../../constants/theme';
 import Input from '../../../components/Input';
 import {Height} from '../../../constants/dimensions';
 import Button from '../../../components/Button';
 import Icon from '../../../components/Icon';
+import {isIOS} from '../../../constants/platform';
 
 const Login = ({navigation}) => {
   const [email, setEmail] = useState('');
@@ -22,45 +33,83 @@ const Login = ({navigation}) => {
 
   const checkDisabled = email === '' || password === '';
 
+  const loginWith = provider => {
+    console.log('Login with', provider);
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.pageTitle}>Welcome Back!</Text>
-        <Text style={styles.noAccountContainer}>
-          Don't you have an account ?{' '}
-          <Text onPress={handleNavigateSignUp} style={styles.signUpText}>
-            Sign Up!
+    <TouchableWithoutFeedback onPress={keyboardDismiss}>
+      <ScrollView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.pageTitle}>Welcome Back!</Text>
+          <Text style={styles.noAccountContainer}>
+            Don't you have an account ?{' '}
+            <Text onPress={handleNavigateSignUp} style={styles.signUpText}>
+              Sign Up!
+            </Text>
           </Text>
-        </Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <Input
-          label="Email"
-          placeholder="Enter your email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          icon={<Icon name="email" size={20} />}
-        />
-        <Input
-          value={password}
-          onChangeText={text => setPassword(text)}
-          secureTextEntry={true}
-          icon={<Icon name={showPassword ? 'eye-off' : 'eye'} size={20} />}
-          iconPressed={() => {
-            setShowPassword(!showPassword);
-          }}
-          label="Password"
-          placeholder="Enter your password"
-        />
-      </View>
-      <View style={styles.registerBtn}>
-        <Button
-          isDisabled={checkDisabled}
-          title="Login"
-          onPress={handleRegister}
-        />
-      </View>
-    </SafeAreaView>
+        </View>
+        <KeyboardAvoidingView behavior={isIOS ? 'padding' : 'height'}>
+          <View style={styles.inputContainer}>
+            <Input
+              label="Email"
+              placeholder="Enter your email"
+              value={email}
+              onChangeText={text => setEmail(text)}
+              icon={<Icon name="email" size={20} />}
+              autoCapitalize="none"
+              autoCorrect={false}
+              textContentType="emailAddress"
+              keyboardType="email-address"
+            />
+            <Input
+              value={password}
+              onChangeText={text => setPassword(text)}
+              secureTextEntry={true}
+              icon={<Icon name={showPassword ? 'eye-off' : 'eye'} size={20} />}
+              iconPressed={() => {
+                setShowPassword(!showPassword);
+              }}
+              label="Password"
+              placeholder="Enter your password"
+            />
+          </View>
+          <View style={styles.registerBtn}>
+            <Button
+              isDisabled={checkDisabled}
+              title="Login"
+              onPress={handleRegister}
+            />
+          </View>
+        </KeyboardAvoidingView>
+        <TouchableOpacity style={styles.forgotPasswordContainer}>
+          <Text style={styles.forgotPasswordText}>Forgot your Password ?</Text>
+        </TouchableOpacity>
+        <View style={styles.orLoginSeparatorContainer}>
+          <View style={styles.orLoginSeparator} />
+          <Text style={styles.orLoginText}>Or Login Using</Text>
+          <View style={styles.orLoginSeparator} />
+        </View>
+        <View style={styles.loginWithContainer}>
+          <TouchableOpacity
+            style={styles.loginWithButton}
+            onPress={() => {
+              loginWith('apple');
+            }}>
+            <Icon name="apple" size={30} />
+            <Text style={styles.loginWithButtonText}>Apple</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginWithButton}>
+            <Icon name="google" size={30} />
+            <Text style={styles.loginWithButtonText}>Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginWithButton}>
+            <Icon name="facebook" size={30} />
+            <Text style={styles.loginWithButtonText}>Facebook</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -70,6 +119,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.background,
+    paddingVertical: 60,
   },
   header: {
     paddingHorizontal: theme.spacing.md,
@@ -94,9 +144,50 @@ const styles = StyleSheet.create({
     marginTop: Height * 0.15,
   },
   registerBtn: {
-    bottom: 30,
-    position: 'absolute',
+    marginTop: theme.spacing.lg,
     width: '100%',
-    marginBottom: theme.spacing.xl,
+  },
+  forgotPasswordContainer: {
+    alignItems: 'center',
+    marginTop: theme.spacing.lg,
+  },
+  forgotPasswordText: {
+    color: theme.colors.primary,
+    fontWeight: '600',
+  },
+  orLoginSeparatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: theme.spacing.xl,
+  },
+  orLoginSeparator: {
+    height: 1,
+    backgroundColor: theme.palette.softWhite,
+    flex: 1,
+    marginHorizontal: theme.spacing.lg,
+  },
+  orLoginText: {
+    color: theme.colors.text,
+    fontSize: theme.fontSize.xs,
+    fontWeight: '400',
+  },
+  loginWithContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: theme.spacing.xl,
+  },
+  loginWithButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  loginWithButtonText: {
+    color: theme.colors.text,
+    fontSize: theme.fontSize.sm,
+    fontWeight: '600',
+    marginTop: theme.spacing.sm,
   },
 });
